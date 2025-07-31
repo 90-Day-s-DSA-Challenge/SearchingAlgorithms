797a5c7549f7b417ba53e5c4a7484b7254d84f5a
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-07-31 13:36:48 +0530

    single element in sorted array, leetcode, medium, binary search

---
+++ src/SingleElementInSortedArray.java
@@ -0,0 +1,34 @@
+public class SingleElementInSortedArray {
+//    540. Single Element in a Sorted Array
+//    https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+        System.out.println(singleNonDuplicate(nums));
+
+    }
+
+    public static int singleNonDuplicate(int[] nums){
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start < end){
+            int mid = start + (end - start) / 2;
+
+//           forcefully convert mid to even val
+            if(mid % 2 == 1){
+                mid--;
+            }
+
+//            when nums[mid] == nums[mid+1] then set start = mid+2
+            if(nums[mid] == nums[mid+1]){
+                start = mid + 2;
+            }else{
+                end = mid;
+            }
+        }
+
+        return nums[start];
+
+    }
+}
